3e5fae09a722fa843637437e73e78d261f53c0c9
lixingni <xingni.li@newsbreak.com>
2021-09-24 15:34:05 +0800

    fix

--- conf/global.sh
+++ conf/global.sh
@@ -1,5 +1,6 @@
 
 PATH_PREFIX="/home/services/lixingni/workspace/blendOnlineUpdate/"
+PYTHON3="/home/services/miniconda3/bin/python3"
 
 ALARM_EMAILS="xingni.li@newsbreak.com"
 CONF_PREFIX="${PATH_PREFIX}/conf/"
@@ -13,4 +14,3 @@ MID_FILE="${DATA_PREFIX}/qratioCopc.txt.mid"
 EXP_CONF_FILE="${CONF_PREFIX}/exp.conf"
 
 CURR_DATE=`date -d last-day +%Y-%m-%d`
-#CURR_DATE="2021-07-14"
